786ae00802a394a19752debc5ca88cfb8f0116c1
Abhinav Rajesh <abhinavrajesh49@gmail.com>
2022-02-25 14:54:20 +0530

    Changed eslint rule

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -3,9 +3,7 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <div>
-      <h1 key="No idea why this needs a key @ajal333 please check :)">
-        Let&apos;s go?
-      </h1>
+      <h1>Let&apos;s go?</h1>
     </div>
   );
 };
