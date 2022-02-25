f61f05d30d78955089152c5a2c0cd70ee10f8454
Abhinav Rajesh <abhinavrajesh49@gmail.com>
2022-02-25 14:09:44 +0530

    Added the basic monorepo, tailwindcss and truffle

---
+++ packages/web/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1 key="No idea why this needs a key @ajal333 please check :)">
+        Let&apos;s go?
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
