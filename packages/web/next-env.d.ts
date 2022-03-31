49943e8db931640a8c704f0e3933f76bfd5918bb
Abhinav Rajesh <abhinavrajesh49@gmail.com>
2022-03-31 10:18:55 +0530

    Kinda Added web3

--- packages/web/next-env.d.ts
+++ packages/web/next-env.d.ts
@@ -1,5 +1,15 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 
+import { MetaMaskInpageProvider } from "@metamask/providers";
+import Web3 from "web3";
+
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
+
+declare global {
+  interface Window {
+    web3: Web3;
+    ethereum: MetaMaskInpageProvider;
+  }
+}
--- packages/web/pages/_app.tsx
+++ packages/web/pages/_app.tsx
@@ -2,10 +2,26 @@ import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import web3 from "@infrastructure/web3";
+import DCrowdfundingABI from "@public/DCrowdfunding.json";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState<QueryClient>(() => new QueryClient());
+
+  useEffect(() => {
+    loadBlockchain();
+  }, []);
+
+  const loadBlockchain = async () => {
+    const networkId = await web3.eth.net.getId();
+    if (networkId === 44787) {
+      await web3.eth.Contract(
+        DCrowdfundingABI,
+        "0xd9145CCE52D386f254917e481eB44e9943F39138"
+      );
+    }
+  };
 
   return (
     <QueryClientProvider client={queryClient}>
