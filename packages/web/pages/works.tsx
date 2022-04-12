2d059d8628dc7c4d2dc2d06e29ce2350218ba5c3
Ajal <ajapoos1999@gmail.com>
2022-04-12 23:52:49 +0530

    how it works page changes|

--- packages/web/pages/works.tsx
+++ packages/web/pages/works.tsx
@@ -4,6 +4,24 @@ import { H2 } from "@presentation/common/Typography";
 import Layout from "@presentation/Layout";
 
 const Work = () => {
+  const Works = [
+    {
+      title: "Create a Campaign for Fundraising",
+      description:
+        "It'll take only 2 minutes. Just enter a few details about the funds you are raising for.",
+    },
+    {
+      title: "Share your Campaign",
+      description:
+        "All you need to do is share the Campaign with your friends, family and others. In no time, support will start pouring in.",
+    },
+    {
+      title: "Request and Withdraw Funds",
+      description:
+        "The funds raised can be withdrawn directly to the recipient when 50% of the contributors approve of the Withdrawal Request.",
+    },
+  ];
+
   return (
     <Layout>
       <HeadMeta
@@ -15,15 +33,9 @@ const Work = () => {
       />
       <H2 className="mt-[50px] mb-[40px]">How it works?</H2>
       <div className="grid grid-rows-1 md:grid-cols-2 lg:grid-cols-3 gap-[45px]">
-        {Array(3)
-          .fill(null)
-          .map((_, idx) => (
-            <HelpCard
-              title="Create a Campaign for Fundraising"
-              description="It'll take only 2 minutes. Just enter a few details about the funds you are raising for."
-              key={idx}
-            />
-          ))}
+        {Works?.map(({ title, description }, idx) => (
+          <HelpCard title={title} description={description} key={idx} />
+        ))}
       </div>
     </Layout>
   );
--- packages/web/presentation/Footer/index.tsx
+++ packages/web/presentation/Footer/index.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const Footer = () => {
   return (
     <footer className="bg-[#F5F5F5] pt-[50px]">
@@ -5,10 +7,18 @@ const Footer = () => {
         <div className="flex justify-between w-full mb-[32px]">
           <h2>D-Crowdfunding</h2>
           <ul className="flex flex-col">
-            <li>Home</li>
-            <li>How it works?</li>
-            <li>Create Campaigns</li>
-            <li>Active Campaigns</li>
+            <Link href="/" passHref>
+              <li className="cursor-pointer">Home</li>
+            </Link>
+            <Link href="/works" passHref>
+              <li className="cursor-pointer">How it works?</li>
+            </Link>
+            <Link href="/create" passHref>
+              <li className="cursor-pointer">Create Campaign</li>
+            </Link>
+            <Link href="/campaigns" passHref>
+              <li className="cursor-pointer">Active Campagins</li>
+            </Link>
           </ul>
         </div>
         <div className="flex py-[10px] border-t border-gray-300 items-center justify-center text-[14px] font-bold">
