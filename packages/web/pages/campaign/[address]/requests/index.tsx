67ca0e8f0c5b478b75694e143297fcab3ed7c9f7
Ajal <ajapoos1999@gmail.com>
2022-04-19 02:22:29 +0530

    added ui for voting

--- packages/web/pages/campaign/[address]/requests/index.tsx
+++ packages/web/pages/campaign/[address]/requests/index.tsx
@@ -2,13 +2,23 @@ import type { GetServerSideProps, GetServerSidePropsContext } from "next";
 import React, { useEffect, useState } from "react";
 
 import HeadMeta from "@presentation/common/HeadMeta";
-import { H2, H3 } from "@presentation/common/Typography";
+import { H2 } from "@presentation/common/Typography";
 import Button from "@presentation/common/Button";
 import Layout from "@presentation/Layout";
 
 import CrowdFunding from "@infrastructure/crowdfunding";
 import { getMaticPrice } from "@infrastructure/getMaticToUSD";
 import web3 from "@infrastructure/web3";
+import {
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+  useToast,
+} from "@chakra-ui/react";
 
 interface Props {
   campaignId: string;
@@ -34,6 +44,10 @@ export default function Requests({
 }: Props) {
   const [data, setData] = useState<RequestInterface[]>([]);
   const [account, setAccount] = useState<string>();
+  const [voting, setVoting] = useState<boolean>(false);
+  const [withdrawing, setWithdrawing] = useState<boolean>(false);
+
+  const toast = useToast();
 
   useEffect(() => {
     getRequests();
@@ -57,26 +71,64 @@ export default function Requests({
   };
 
   const handleVote = async (requestId: number) => {
+    setVoting(true);
     try {
       const campaign = CrowdFunding(campaignId);
       const response = await campaign.methods.voteRequest(requestId).send({
         from: account,
       });
       console.log(response);
+      toast({
+        title: "Voting completed.",
+        description: `You have sucessfully voted for this campaign.`,
+        status: "success",
+        duration: 9000,
+        isClosable: true,
+        position: "top",
+      });
     } catch (error) {
       console.log(error);
+      toast({
+        title: "Voting unsucessful.",
+        description: `Voting could not be completed sucessfully.`,
+        status: "error",
+        duration: 9000,
+        isClosable: true,
+        position: "top",
+      });
+    } finally {
+      setVoting(false);
     }
   };
 
   const makePayment = async (requestId: number) => {
+    setWithdrawing(true);
     try {
       const campaign = CrowdFunding(campaignId);
       const response = await campaign.methods.makePayment(requestId).send({
         from: account,
       });
+      toast({
+        title: "Withdrawal completed.",
+        description: `You have sucessfully completed the fund withdrawal.`,
+        status: "success",
+        duration: 9000,
+        isClosable: true,
+        position: "top",
+      });
       console.log(response);
     } catch (error) {
       console.log(error);
+      toast({
+        title: "Fund Withdrawal unsucessful.",
+        description: `Fund withdrawal could not be completed sucessfully.`,
+        status: "error",
+        duration: 9000,
+        isClosable: true,
+        position: "top",
+      });
+    } finally {
+      setWithdrawing(false);
     }
   };
 
@@ -91,16 +143,51 @@ export default function Requests({
           url=""
         />
         <H2 className="mt-[50px]">Requests</H2>
-        <div className="flex flex-col max-w-[500px] mx-auto mt-[48px]">
-          {data?.map((d, i) => (
-            <>
-              <H3>{d.description}</H3>
-              <Button onClick={() => handleVote(i)}>Vote</Button>
-              {account === organisationAddress && (
-                <Button onClick={() => makePayment(i)}>Pull funds</Button>
-              )}
-            </>
-          ))}
+        <div className="flex flex-col  mx-auto mt-[48px]">
+          <TableContainer>
+            <Table variant="striped" colorScheme="teal">
+              <Thead>
+                <Tr>
+                  <Th>Description</Th>
+                  <Th>Recipient Address</Th>
+                  <Th>Amount</Th>
+                  <Th>Voters</Th>
+                  <Th>Actions</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {data?.map((d, i) => (
+                  <Tr>
+                    <Td>{d?.description}</Td>
+                    <Td>{d?.recipient}</Td>
+                    <Td>{d?.value}</Td>
+                    <Td>{d?.noOfVoters}</Td>
+                    <Td>
+                      {account === organisationAddress && (
+                        <Button
+                          isLoading={withdrawing}
+                          loadingText="Pulling funds..."
+                          onClick={() => makePayment(i)}
+                          className="mr-4"
+                          colorScheme="linkedin"
+                        >
+                          Pull funds
+                        </Button>
+                      )}
+                      <Button
+                        isLoading={voting}
+                        loadingText="Voting..."
+                        size="sm"
+                        onClick={() => handleVote(i)}
+                      >
+                        Vote
+                      </Button>
+                    </Td>
+                  </Tr>
+                ))}
+              </Tbody>
+            </Table>
+          </TableContainer>
         </div>
       </Layout>
     </>
